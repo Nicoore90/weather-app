b085b6ff6142cf143141c45a34e621d04620affa
nrmerlis <nicoxeneize.10@gmail.com>
2022-05-26 17:03:36 -0300

    - Cambios en el proyecto para corrección!

--- src/App.js
+++ src/App.js
@@ -1,64 +1,71 @@
 import './App.css';
-import { Header,Dropdown } from './components/header/header';
-import { useEffect, useState } from 'react';
+import {Header, Dropdown} from './components/header/header';
+import {useEffect, useState} from 'react';
 
 function App() {
-  let lat = -31
-  let long = -64
 
-  function getGeolocation() {
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
+    const [coordinates, setCoordinates] = useState({lat: 31, long: 61});
 
-    
-    navigator.geolocation.getCurrentPosition((position) => {
-      let coords = position.coords
-      console.log(coords)
-      lat = coords.latitude
-      long = coords.longitude
-      console.log(lat)
-      console.log(long)
-    }, (error) => {
-      console.log(error)
-    }, options)
-  }
+    const [weather, setWeather] = useState();
 
-  getGeolocation()
+    const [error, setError] = useState(false);
 
-  console.log(lat)
-  console.log(long)
+    const api = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.long}&units=metric&appid=5dd9a1d9e53e58a109d3b54088156c0a`
 
-  const api = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=metric&appid=94d76cede76928778bd75e39b3d98944`
-  const [weather, setWeather] = useState()
+    function fetchCoordinates() {
 
+        const options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
 
- 
-  useEffect(() => {
-    fetch(api)
-    .then((res) => res.json())
-    .then((data) => {
-      setWeather(data)
-    })
-  }, [])
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setCoordinates({lat: position.coords.latitude, long: position.coords.longitude});
+            },
+            (error) => {
+                console.log(error)
+            }, options)
 
-  return (
-    <div className="App">
-      <Header>
-        <Dropdown />
-      </Header>
-      {weather ? (
-        <div className='weather__container'>
-          <h1 className='timezone'>{weather.timezone}</h1>
-          <h2 className='temp'>{weather.current.temp}°C</h2>
-          <h3 className='weather'>{weather.current.weather[0].main}</h3>
-          <p className='description'>{weather.current.weather[0].description}</p>
+    }
+
+    useEffect(() => {
+        fetchCoordinates();
+    }, [])
+
+    function fetchWeather(){
+        fetch(api)
+            .then((res) => {
+                if (res.status <= 300 && res.status >= 200)
+                    return res.json()
+                setError(true);
+            })
+            .then((data) => {
+                setWeather(data)
+            })
+    }
+
+    useEffect(() => {
+        fetchWeather();
+    }, [coordinates])
+
+    return (
+        <div className="App">
+            <Header>
+                <Dropdown/>
+            </Header>
+            {weather ? (
+                <div className='weather__container'>
+                    <h1 className='timezone'>{weather.timezone}</h1>
+                    <h2 className='temp'>{weather.current.temp}°C</h2>
+                    <h3 className='weather'>{weather.current.weather[0].main}</h3>
+                    <p className='description'>{weather.current.weather[0].description}</p>
+                </div>
+            ) : (error ? <p>Error cargando la temperatura</p> : <p className='center'>Cargando....</p>)
+            }
         </div>
-      ) : null}
-    </div>
-  );
+    );
 }
 
 export default App;
